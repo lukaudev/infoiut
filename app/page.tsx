8ffbd07ae9dd58dabaa103d3b61cec53339daaea
Lukau NDOMBELE <72668885+lukaudev@users.noreply.github.com>
2026-06-17 08:48:01 +0000

    feat: Amélioration des boutons de navigation avec des icônes et ajustements de style

--- app/page.tsx
+++ app/page.tsx
@@ -74,12 +74,15 @@ export default function Home() {
                 className="flex gap-4"
               >
                 <Link href="#programmes">
-                  <button className="text-md bg-teal-600 cursor-pointer hover:bg-teal-700 text-white px-8 py-3 rounded-lg font-semibold transition-colors flex items-center gap-2">
+                  <button className="text-md bg-teal-600 cursor-pointer hover:bg-teal-700 text-white px-8 py-3 rounded-lg font-semibold transition-colors flex items-center">
                     Découvrez mon parcours  <span>→</span>
                   </button>
                 </Link>
-                <button className="text-md border-2 cursor-pointer border-gray-300 text-gray-700 hover:border-gray-400 px-8 py-3 rounded-lg font-semibold transition-colors">
-                  ↓ Télécharger mon rapport
+                <button className="text-md border-2 cursor-pointer border-gray-300 text-gray-700 hover:border-gray-400 px-8 py-3 rounded-lg font-semibold transition-colors flex items-center gap-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+</svg>
+                   <span>Télécharger mon rapport</span>
                 </button>
               </motion.div>
             </motion.div>
